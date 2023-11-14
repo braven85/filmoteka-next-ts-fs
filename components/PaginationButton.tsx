37453364f6b817f6bb162cc7a94ebf4b38289916
Radek <radekie@wp.pl>
2023-11-14 12:05:11 +0100

    Accessibility updates

--- components/PaginationButton.tsx
+++ components/PaginationButton.tsx
@@ -5,11 +5,21 @@ interface PaginationButtonProps {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
+  id?: string;
+  label?: string;
 }
 
-const PaginationButton: React.FC<PaginationButtonProps> = ({ children, className, onClick }) => {
+const PaginationButton: React.FC<PaginationButtonProps> = ({
+  children,
+  className,
+  onClick,
+  id,
+  label,
+}) => {
   return (
     <button
+      id={id}
+      aria-label={label}
       onClick={onClick}
       className={twMerge(
         `w-[40px] h-[40px] rounded-md flex justify-center items-center`,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       keyframes: {
-        "accordion-down": {
+        'accordion-down': {
           from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
       colors: {
-        "link-underline": "#FF6B08",
-        "movie-genres-and-date": "#FF6B08",
-        "active-pagination-bg": "#FF6B08",
-        "pagination-btn-bg": "#F7F7F7",
-        "modal-text-gray": "#8C8C8C",
-        "vote-average-bg": "#FF6B01",
-        "active-button-bg": "#FF6B01",
-        "footer-text": "#545454",
+        'link-underline': '#FF6B08',
+        'movie-genres-and-date': '#AD1500',
+        'active-pagination-bg': '#FF6B08',
+        'pagination-btn-bg': '#F7F7F7',
+        'modal-text-gray': '#8C8C8C',
+        'vote-average-bg': '#FF6B01',
+        'active-button-bg': '#FF6B01',
+        'footer-text': '#545454',
       },
       backgroundImage: {
-        "main-page": "linear-gradient(rgba(0,0,0,0.56), rgba(0,0,0,0.56)), url('/bgImg1.png')",
-        "library-page": "linear-gradient(rgba(0,0,0,0.56), rgba(0,0,0,0.56)), url('/bgImg2.png')",
+        'main-page': "linear-gradient(rgba(0,0,0,0.56), rgba(0,0,0,0.56)), url('/bgImg1.png')",
+        'library-page': "linear-gradient(rgba(0,0,0,0.56), rgba(0,0,0,0.56)), url('/bgImg2.png')",
       },
       backgroundPosition: {
-        "left-200": "-200px 0px",
+        'left-200': '-200px 0px',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
